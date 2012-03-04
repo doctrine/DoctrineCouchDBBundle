7871f6838a3b151f8956d31dfe8d84f1c1e04226
Benjamin Eberlei <kontakt@beberlei.de>
2012-03-04 19:29:25 +0100

    Update composer.json and fix tests

--- Tests/BundleTest.php
+++ Tests/BundleTest.php
@@ -28,15 +28,16 @@ class BundleTest extends TestCase
     public function testRegisterCompilerPasses()
     {
         $bundle = new DoctrineCouchDBBundle();
-        $builder = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder');
-        
-        $builder->expects($this->at(0))
+        $builder = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder', array('hasExtension', 'addCompilerPass'));
+        $builder->expects($this->at(0))->method('hasExtension')->will($this->returnValue(false));
+
+        $builder->expects($this->at(1))
                 ->method('addCompilerPass')
                 ->with(
                     $this->isInstanceOf('Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\RegisterEventListenersAndSubscribersPass'),
                     $this->equalTo(PassConfig::TYPE_BEFORE_OPTIMIZATION)
                 );
-        $builder->expects($this->at(1))
+        $builder->expects($this->at(2))
                 ->method('addCompilerPass')
                 ->with(
                     $this->isInstanceOf('Symfony\Bridge\Doctrine\DependencyInjection\CompilerPass\DoctrineValidationPass'),
